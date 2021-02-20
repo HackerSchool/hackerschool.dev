62ac3106256a56aa0dedea7a545bfb9071c63680
Hugo Marques <39102634+Hugo-Marques-work@users.noreply.github.com>
2021-02-20 03:02:07 +0000

    FirstIteration (member tab) and Second Iteration (project tab) overhauls (mixed together, forgot a commit)

---
+++ content/projetos/current/HSTable.md
@@ -0,0 +1,14 @@
++++
+title = "HSTable"
+description= "Uma mesa tátil criada a partir de raíz usando scanners e raspberry pi como controlador." 
+
+date = "2020-10-14" 
+Destacado=false 
+
+photo = "hsTable.jpeg" 
+
+type = "currentProject" 
+layout = "projeto" 
+
+teamPhotos = ["anaAjuda.png","mariaPortugal.png","afonsoCerto.jpg" ] 
++++
---
+++ projectImageCompressor.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import PIL
+import sys, os
+import shutil 
+from pathlib import Path
+
+
+def copyAndCompress(fromDirectory, compressDirectory):
+    shutil.rmtree(compressDirectory)
+    print(fromDirectory)
+
+    shutil.copytree(fromDirectory, compressDirectory) 
+    print(compressDirectory)
+    print(os)
+    print(os.chdir(compressDirectory))
+    os.chdir(compressDirectory)
+    images = [file for file in os.listdir() if file.endswith(('jpg', 'png' ))]
+    print( images)
+    print(os.listdir())
+    for image in images:
+        # 1. Open the image
+        img = Image.open(image)
+        img.thumbnail((128,128), Image.ANTIALIAS)
+        # 2. Compressing the image
+        img.save(image,
+                optimize=True,
+                quality=100)
+
+base_path = Path(__file__).parent
+print("test")
+
+fromDirectory =  (base_path / "projectImagesBeforeCompress").resolve()
+compressDirectory =  (base_path / "projectImagesAfterCompress").resolve()
+
+copyAndCompress(fromDirectory, compressDirectory)
